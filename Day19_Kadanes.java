17d3bce614b6986826f4cb01a6d6c92e59629427
Kanishka Hirapure <kannuhirapure72@gmail.com>
2025-07-05 13:12:25 +0530

    new code

--- Day19_Kadanes.java
+++ Day19_Kadanes.java
@@ -69,3 +69,63 @@ public class Day19_Kadanes {
 //         int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3};
 //         kadanes(numbers);
 //     }
+
+
+// Q2. Find the Start and End Index of Maximum Sum Subarray
+
+// public class Day19_Kadanes {
+//     public static int kadanes(int[] arr) {
+//         int maxSum = Integer.MIN_VALUE;
+//         int currSum = 0;
+
+//         int start = 0;         // Final subarray start index
+//         int end = 0;           // Final subarray end index
+//         int tempStart = 0;     // Temporary start index
+
+//         for (int i = 0; i < arr.length; i++) {
+//             currSum += arr[i];
+
+//             if (currSum > maxSum) {
+//                 maxSum = currSum;
+//                 start = tempStart;
+//                 end = i;
+//             }
+
+//             if (currSum < 0) {
+//                 currSum = 0;
+//                 tempStart = i + 1;
+//             }
+//         }
+
+//         // Print the subarray
+//         System.out.print("Subarray with max sum: ");
+//         for (int i = start; i <= end; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+
+//         System.out.println("Start index = " + start);
+//         System.out.println("End index = " + end);
+
+//         return maxSum;
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+//         int result = kadanes(arr);
+//         System.out.println("Maximum subarray sum is: " + result);
+//     }
+// }
+
+
+// output
+
+// Subarray with max sum: 4 -1 2 1 
+// Start index = 3  
+// End index = 6  
+// Maximum subarray sum is: 6
+
+
+
+
+
